8cbee37096c3592803e88551e04b069ae4bcc3d8
Welisson Silveira <welissonmwse@gmail.com>
2020-07-28 14:03:49 -0300

    ...

--- src/Pages/cadastro/Categoria/index.js
+++ src/Pages/cadastro/Categoria/index.js
@@ -6,6 +6,16 @@ function Categoria(){
     return(
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
+
+            <form>
+                <label>
+                    Nome da Categoria
+                    <input type="text" />
+                </label>
+                <button>
+                    Cadastrar
+                </button>
+            </form>
             <Link to="/" >
                 Ir para home
             </Link>
