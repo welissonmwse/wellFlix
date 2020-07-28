10949a32a55616df82551b84340812e505d3f935
Welisson Silveira <welissonmwse@gmail.com>
2020-07-28 13:25:10 -0300

    Alteração no Carousel

---
+++ src/components/PageDefault/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Menu from '../Menu'
+import Footer from '../Footer'
+import Main from './styles';
+
+
+function PageDefault({children}){
+    return(
+        <>
+            <Menu />
+            <Main>
+                {children}
+            </Main>
+            <Footer/>
+        </>
+    )
+}
+
+export default PageDefault;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import './index.css';
-import App from './App';
+import Home from '../src/Pages/Home';
+import CadastroVideo from './Pages/cadastro/Video'
+import Categoria from './Pages/cadastro/Categoria'
 
+const Pagina404 = () => (<div>Página404</div>)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={Home} exact />
+      <Route path='/cadastro/video' component={CadastroVideo} />
+      <Route path='/cadastro/categoria' component={Categoria} />
+      <Route component={Pagina404}  />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
